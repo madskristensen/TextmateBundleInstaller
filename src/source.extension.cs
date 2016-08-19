55270df0ccc62d52d8ac0dfedfa43957064e0013
Mads Kristensen <post@madskristensen.net>
2016-08-19 13:20:31 -0700

    Updates

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -9,7 +9,7 @@ namespace TextmateBundleInstaller
     {
         public const string Id = "4773ce75-6f30-4269-9557-1f7c30a47be2";
         public const string Name = "TextMate Bundle Installer";
-        public const string Description = "Adds basic language support for a wide variety of programming languages such as Clojure, Go, Jade, Lua, Swift, Ruby and many more";
+        public const string Description = "Adds basic language support for a wide variety of programming languages such as Clojure, Go, Jade, Lua, Swift, Ruby and many more...";
         public const string Language = "en-US";
         public const string Version = "1.0";
         public const string Author = "Mads Kristensen";
